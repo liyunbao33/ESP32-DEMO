748be07e0060dbc1193ba5bf1b625bc282d55a72
liyunbao33 <liyunbao33@163.com>
2023-03-04 22:31:18 +0800

    2023.3.4 22:31

--- sntp/main/sntp_example_main.c
+++ sntp/main/sntp_example_main.c
@@ -46,7 +46,7 @@ void time_sync_notification_cb(struct timeval *tv)
     ESP_LOGI(TAG, "Notification of a time synchronization event");
 }
 
-void app_main(void)
+void get_time(void)
 {
     ++boot_count;
     ESP_LOGI(TAG, "Boot count: %d", boot_count);
@@ -56,32 +56,33 @@ void app_main(void)
     time(&now);
     localtime_r(&now, &timeinfo);
     // Is time set? If not, tm_year will be (1970 - 1900).
-    if (timeinfo.tm_year < (2016 - 1900)) {
+    if (timeinfo.tm_year < (2016 - 1900))
+    {
         ESP_LOGI(TAG, "Time is not set yet. Connecting to WiFi and getting time over NTP.");
         obtain_time();
         // update 'now' variable with current time
         time(&now);
     }
-// #ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
-//     else {
-//         // add 500 ms error to the current system time.
-//         // Only to demonstrate a work of adjusting method!
-//         {
-//             ESP_LOGI(TAG, "Add a error for test adjtime");
-//             struct timeval tv_now;
-//             gettimeofday(&tv_now, NULL);
-//             int64_t cpu_time = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
-//             int64_t error_time = cpu_time + 500 * 1000L;
-//             struct timeval tv_error = { .tv_sec = error_time / 1000000L, .tv_usec = error_time % 1000000L };
-//             settimeofday(&tv_error, NULL);
-//         }
+    // #ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
+    //     else {
+    //         // add 500 ms error to the current system time.
+    //         // Only to demonstrate a work of adjusting method!
+    //         {
+    //             ESP_LOGI(TAG, "Add a error for test adjtime");
+    //             struct timeval tv_now;
+    //             gettimeofday(&tv_now, NULL);
+    //             int64_t cpu_time = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
+    //             int64_t error_time = cpu_time + 500 * 1000L;
+    //             struct timeval tv_error = { .tv_sec = error_time / 1000000L, .tv_usec = error_time % 1000000L };
+    //             settimeofday(&tv_error, NULL);
+    //         }
 
-//         ESP_LOGI(TAG, "Time was set, now just adjusting it. Use SMOOTH SYNC method.");
-//         obtain_time();
-//         // update 'now' variable with current time
-//         time(&now);
-//     }
-// #endif
+    //         ESP_LOGI(TAG, "Time was set, now just adjusting it. Use SMOOTH SYNC method.");
+    //         obtain_time();
+    //         // update 'now' variable with current time
+    //         time(&now);
+    //     }
+    // #endif
 
     char strftime_buf[64];
 
@@ -99,6 +100,8 @@ void app_main(void)
     strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
     ESP_LOGI(TAG, "The current date/time in Shanghai is: %s", strftime_buf);
 
+    // vTaskDelay(5000 / portTICK_PERIOD_MS);
+
     // if (sntp_get_sync_mode() == SNTP_SYNC_MODE_SMOOTH) {
     //     struct timeval outdelta;
     //     while (sntp_get_sync_status() == SNTP_SYNC_STATUS_IN_PROGRESS) {
@@ -111,16 +114,16 @@ void app_main(void)
     //     }
     // }
 
-    const int deep_sleep_sec = 10;
-    ESP_LOGI(TAG, "Entering deep sleep for %d seconds", deep_sleep_sec);
-    esp_deep_sleep(1000000LL * deep_sleep_sec);
+    // const int deep_sleep_sec = 10;
+    // ESP_LOGI(TAG, "Entering deep sleep for %d seconds", deep_sleep_sec);
+    // esp_deep_sleep(1000000LL * deep_sleep_sec);
 }
 
-static void obtain_time(void)
+void app_main(void)
 {
-    ESP_ERROR_CHECK( nvs_flash_init() );
+    ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK( esp_event_loop_create_default() );
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
      * Read "Establishing Wi-Fi or Ethernet Connection" section in
@@ -130,19 +133,41 @@ static void obtain_time(void)
 
     initialize_sntp();
 
+    while (1)
+    {
+        get_time();
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+}
+
+static void obtain_time(void)
+{
+    // ESP_ERROR_CHECK(nvs_flash_init());
+    // ESP_ERROR_CHECK(esp_netif_init());
+    // ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    // /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+    //  * Read "Establishing Wi-Fi or Ethernet Connection" section in
+    //  * examples/protocols/README.md for more information about this function.
+    //  */
+    // ESP_ERROR_CHECK(example_connect());
+
+    // initialize_sntp();
+
     // wait for time to be set
     time_t now = 0;
-    struct tm timeinfo = { 0 };
+    struct tm timeinfo = {0};
     int retry = 0;
     const int retry_count = 10;
-    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count) {
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count)
+    {
         ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
     time(&now);
     localtime_r(&now, &timeinfo);
 
-    ESP_ERROR_CHECK( example_disconnect() );
+    ESP_ERROR_CHECK(example_disconnect());
 }
 
 static void initialize_sntp(void)
