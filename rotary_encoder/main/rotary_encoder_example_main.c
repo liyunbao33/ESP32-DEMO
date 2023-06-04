a73a3055ad8cdb2ad09726983a0ed570052e8b87
liyunbao33 <liyunbao33@163.com>
2023-06-04 20:12:07 +0800

    2023.6.4 20:12

--- rotary_encoder/main/rotary_encoder_example_main.c
+++ rotary_encoder/main/rotary_encoder_example_main.c
@@ -10,6 +10,7 @@
 #include "esp_log.h"
 #include "driver/pulse_cnt.h"
 #include "filter_algorithm.h"
+#include "driver/mcpwm_prelude.h"
 
 static const char *TAG = "example";
 
@@ -18,6 +19,10 @@ static const char *TAG = "example";
 
 #define EXAMPLE_EC11_GPIO_A 16
 #define EXAMPLE_EC11_GPIO_B 4
+
+#define PULSE_GPIO             0        // GPIO connects to the PWM signal line
+#define TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1us per tick
+#define TIMEBASE_PERIOD        100    // 20000 ticks, 20ms
 
 // static bool example_pcnt_on_reach(pcnt_unit_handle_t unit, const pcnt_watch_event_data_t *edata, void *user_ctx)
 // {
@@ -29,6 +34,7 @@ static const char *TAG = "example";
 // }
 
 pcnt_unit_handle_t pcnt_unit = NULL;
+mcpwm_cmpr_handle_t comparator = NULL;
 
 static void get_count_task(void *arg)
 {
@@ -54,6 +60,19 @@ static void get_count_task(void *arg)
             // ESP_LOGI(TAG, "Pulse count: %d", pulse_count);
         // }
     }
+}
+
+static void set_motor_pwm_task(void *arg)
+{
+    static int step = 0;
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(5000));
+        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, step));
+        //Add delay, since it takes time for servo to rotate, usually 200ms/60degree rotation under 5V power supply
+        if (step > 100)
+            step = 0;
+        step += 20;
+    }   
 }
 
 static void PCNT_Init(void)
@@ -112,11 +131,57 @@ static void PCNT_Init(void)
 
 static void MCPWM_Init(void)
 {
+    ESP_LOGI(TAG, "Create timer and operator");
+    mcpwm_timer_handle_t timer = NULL;
+    mcpwm_timer_config_t timer_config = {
+        .group_id = 0,
+        .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
+        .resolution_hz = TIMEBASE_RESOLUTION_HZ,
+        .period_ticks = TIMEBASE_PERIOD,
+        .count_mode = MCPWM_TIMER_COUNT_MODE_UP,
+    };
+    ESP_ERROR_CHECK(mcpwm_new_timer(&timer_config, &timer));
 
+    mcpwm_oper_handle_t oper = NULL;
+    mcpwm_operator_config_t operator_config = {
+        .group_id = 0, // operator must be in the same group to the timer
+    };
+    ESP_ERROR_CHECK(mcpwm_new_operator(&operator_config, &oper));
+
+    ESP_LOGI(TAG, "Connect timer and operator");
+    ESP_ERROR_CHECK(mcpwm_operator_connect_timer(oper, timer));
+
+    ESP_LOGI(TAG, "Create comparator and generator from the operator");
+    mcpwm_comparator_config_t comparator_config = {
+        .flags.update_cmp_on_tez = true,
+    };
+    ESP_ERROR_CHECK(mcpwm_new_comparator(oper, &comparator_config, &comparator));
+
+    mcpwm_gen_handle_t generator = NULL;
+    mcpwm_generator_config_t generator_config = {
+        .gen_gpio_num = PULSE_GPIO,
+    };
+    ESP_ERROR_CHECK(mcpwm_new_generator(oper, &generator_config, &generator));
+
+    ESP_LOGI(TAG, "Set generator action on timer and compare event");
+    // go high on counter empty
+    ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_timer_event(generator,
+                    MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH),
+                    MCPWM_GEN_TIMER_EVENT_ACTION_END()));
+    // go low on compare threshold
+    ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_compare_event(generator,
+                    MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparator, MCPWM_GEN_ACTION_LOW),
+                    MCPWM_GEN_COMPARE_EVENT_ACTION_END()));
+
+    ESP_LOGI(TAG, "Enable and start timer");
+    ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
+    ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP));
 }
 
 void app_main(void)
 {
+    MCPWM_Init();
     PCNT_Init();
-    xTaskCreate(get_count_task, "get_count_task", 1024*2, NULL, configMAX_PRIORITIES, NULL);
+    xTaskCreate(get_count_task, "get_count_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
+    xTaskCreate(set_motor_pwm_task, "set_motor_pwm_task", 1024*2, NULL, configMAX_PRIORITIES, NULL);
 }
